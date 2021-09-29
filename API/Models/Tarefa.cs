d8c073143e005af174aa7e5977a188eae842032c
mighty-DOS <h.webprojects@outlook.com>
2021-09-29 12:01:33 -0300

    remover date time

--- API/Models/Tarefa.cs
+++ API/Models/Tarefa.cs
@@ -5,12 +5,11 @@ namespace API.Models
     public class Tarefa
     {
         
-
         public int Id { get; set; }
         public string Nome { get; set; }
         public float Prazo { get; set; }
         public string Descricao { get; set; }
-        public DateTime Conclusao { get; set; }
+        public float Conclusao { get; set; }
 
         public override string ToString() =>
             $"Nome: {Nome} | Prazo: {Prazo} | Descricao: {Descricao} | Conclusao: {Conclusao}";
