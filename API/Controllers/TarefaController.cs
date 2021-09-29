070b495de9da0b1860217d01a44e6251e87ad856
BrunodeLuca-star <brunodeluca403@gmail.com>
2021-09-29 11:49:35 -0300

    att

--- API/Controllers/TarefaController.cs
+++ API/Controllers/TarefaController.cs
@@ -32,45 +32,6 @@ namespace API.Controllers
         [Route("list")]
         public IActionResult List() => Ok(_context.Tarefas.ToList());
 
-        //GET: api/tarefa/getbyid/1
-        [HttpGet]
-        [Route("getbyid/{id}")]
-        public IActionResult GetById([FromRoute] int id)
-        {
-            Tarefa tarefa = _context.Tarefas.Find(id);
-            if (tarefa == null)
-            {
-                return NotFound();
-            }
-            return Ok(tarefa);
-        }
-
-        //DELETE: /api/tarefa/delete/bolacha
-        [HttpDelete]
-        [Route("delete/{name}")]
-        public IActionResult Delete([FromRoute] string name)
-        {
-
-            Tarefa tarefa = _context.Tarefas.FirstOrDefault(tarefa => tarefa.Nome == name);
-
-            if (tarefa == null)
-            {
-                return NotFound();
-            }
-            _context.Tarefas.Remove(tarefa);
-            _context.SaveChanges();
-            return Ok(_context.Tarefas.ToList());
-        }
-
-
-        //PUT: api/tarefa/update
-        [HttpPut]
-        [Route("update")]
-        public IActionResult Update([FromBody] Tarefa tarefa)
-        {
-            _context.Tarefas.Update(tarefa);
-            _context.SaveChanges();
-            return Ok(tarefa);
-        }
+       
     }
 }
