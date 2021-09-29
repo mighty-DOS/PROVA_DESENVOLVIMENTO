37152b8ef800f36cc5bec8da8c4dcb7f553224c6
mighty-DOS <h.webprojects@outlook.com>
2021-09-29 11:25:32 -0300

    main

--- FRONT/src/app/Componets/listar-tarefa/listar-tarefa.component.ts
+++ FRONT/src/app/Componets/listar-tarefa/listar-tarefa.component.ts
@@ -1,3 +1,4 @@
+import { Tarefa } from './../../Models/Tarefa';
 import { Component, OnInit } from "@angular/core";
 import { Tarefa } from "src/app/Models/Tarefa";
 import { TarefaService } from "src/app/Service/Tarefa.service";
@@ -15,7 +16,7 @@ export class ListarTarefaComponent implements OnInit {
     ngOnInit(): void {
         this.service.list().subscribe((tarefas) => {
             this.tarefas = tarefas;
-            for (let produto of tarefas) {
+            for (let tarefa of tarefas) {
                 console.log(tarefas);
             }
         });
