1e0aba0f87fa6fc0c662b07e237ea1699c62f760
mighty-DOS <h.webprojects@outlook.com>
2021-09-29 10:25:25 -0300

    main

--- API/Models/Tarefa.cs
+++ API/Models/Tarefa.cs
@@ -4,6 +4,7 @@ namespace API.Models
 {
     public class Tarefa
     {
+        public Produto() => Conclusao = DateTime.Now;
 
         public int Id { get; set; }
         public string Nome { get; set; }
@@ -11,5 +12,7 @@ namespace API.Models
         public string Descricao { get; set; }
         public DateTime Conclusao { get; set; }
 
+        public override string ToString() =>
+            $"Nome: {Nome} | Prazo: {Prazo} | Conclusao: {Conclusao}";
     }
 }
