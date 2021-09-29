147fd2405cfb7851efe2af38917f1dd3fa9cdd9a
mighty-DOS <h.webprojects@outlook.com>
2021-09-29 09:49:19 -0300

    master

--- API/Models/Tarefa.cs
+++ API/Models/Tarefa.cs
@@ -9,7 +9,7 @@ namespace API.Models
         public string Nome { get; set; }
         public float Prazo { get; set; }
         public string Descricao { get; set; }
-        public DateTime Conclusão { get; set; }
+        public DateTime Conclusao { get; set; }
 
     }
 }
